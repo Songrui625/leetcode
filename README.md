154cdcb9c2d72772e0ab391beefa3067192cceb6
Songrui625 <1029229159@qq.com>
2020-09-03 23:23:16 +0800

    长度最小的子数组

--- README.md
+++ README.md
@@ -15,3 +15,4 @@
 [删除排序数组中的重复项](https://github.com/Songrui625/leetcode/blob/master/src/%E5%8F%8C%E6%8C%87%E9%92%88and%E6%BB%91%E5%8A%A8%E7%AA%97%E5%8F%A3/lc26_%E5%88%A0%E9%99%A4%E6%8E%92%E5%BA%8F%E6%95%B0%E7%BB%84%E4%B8%AD%E7%9A%84%E9%87%8D%E5%A4%8D%E9%A1%B9/Solution.java)  
 [接雨水](https://github.com/Songrui625/leetcode/blob/master/src/%E5%8F%8C%E6%8C%87%E9%92%88and%E6%BB%91%E5%8A%A8%E7%AA%97%E5%8F%A3/lc42_%E6%8E%A5%E9%9B%A8%E6%B0%B4/Solution.java)  
 [长度最小的子数组](https://github.com/Songrui625/leetcode/blob/master/src/%E5%8F%8C%E6%8C%87%E9%92%88and%E6%BB%91%E5%8A%A8%E7%AA%97%E5%8F%A3/lc209_%E9%95%BF%E5%BA%A6%E6%9C%80%E5%B0%8F%E7%9A%84%E5%AD%90%E6%95%B0%E7%BB%84/Solution.java)  
+[]()
---
+++ src/双指针and滑动窗口/lc76_最小覆盖子串/Solution.java
@@ -0,0 +1,53 @@
+package 双指针and滑动窗口.lc76_最小覆盖子串;
+
+import java.util.HashMap;
+
+public class Solution {
+    public String minWindow(String s, String t) {
+        String res = "";
+        int len = Integer.MAX_VALUE;
+        int m = s.length();
+        int n = t.length();
+        int left = 0;
+        int right = 0;
+        int count = 0;
+        HashMap<Character, Integer> window = new HashMap<>();
+        HashMap<Character, Integer> want = new HashMap<>();
+        for (int i = 0; i < t.length(); i++) {
+            Integer integer = want.get(s.charAt(i));
+            integer == null ? 0 : integer++;
+            want.put(s.charAt(i),integer);
+        }
+
+        while (right < m) {
+            char c = s.charAt(right);
+            right++;
+            if (want.get(c) != null) {
+                window.put(c, window.get(c)++);
+                if (window.get(c) == want.get(c)) {
+                    count++;
+                }
+            }
+
+            while (valid == want.size()) {
+                if (right - left < len) {
+                    start = left;
+                    len = right - left;
+                }
+
+                char d = s.charAt(left);
+
+                left++;
+
+                if (want.get(d) != null) {
+                    if (window.get(d) == want.get(d)) {
+                        valid--;
+                    }
+                    window.put(d, window.get(d)--);
+                }
+            }
+        }
+
+        return len ==Integer.MAX_VALUE ? "" :s.subString(start, len);
+    }
+}
