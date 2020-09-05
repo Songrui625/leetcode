70c006d67fca47c74f796969bc354e8bc6488954
Songrui625 <1029229159@qq.com>
2020-09-05 22:46:08 +0800

    新增lc209_长度最小的子数组：暴力求解

--- src/双指针and滑动窗口/lc209_长度最小的子数组/Solution.java
+++ src/双指针and滑动窗口/lc209_长度最小的子数组/Solution.java
@@ -1,37 +1,66 @@
 package 双指针and滑动窗口.lc209_长度最小的子数组;
 
+
+/**
+ * 解题思想一：暴力求解，双重for循环枚举出所有的结果，遇到一次更小的就更新
+ * 解题思想：slide window
+ *
+ */
+
 public class Solution {
 
-    //version:1.0
+    //暴力求解
     public int minSubArrayLen(int s, int[] nums) {
-        if (nums.length == 0) {
-            return 0;
-        }
-        int left, right;
-        int sum = 0;
-        int res = Integer.MAX_VALUE;
-        left = right = 0;
-        for (left = 0; left < nums.length; left++) {
-            sum = 0;
-            if (left == right && nums[left] >= s) {
-                return 1;
-            }
-            sum = nums[left];
-            right = left;
-            while (right < nums.length - 1) {
-                right++;
-                sum += nums[right];
-                if (sum >= s) {
-                    res = Math.min(res, right - left + 1);
+        int len = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                if (sum >= s && len > j - i + 1) {
+                    len = j - i + 1;
                     break;
                 }
             }
         }
-        if (res != Integer.MAX_VALUE) {
-            return res;
-        }
-        return 0;
+
+        return len;
     }
+
+    //version:1.0
+//    public int minSubArrayLen(int s, int[] nums) {
+//        if (nums.length == 0) {
+//            return 0;
+//        }
+//        int left, right;
+//        int sum = 0;
+//        int res = Integer.MAX_VALUE;
+//        left = right = 0;
+//        for (left = 0; left < nums.length; left++) {
+//            sum = 0;
+//            if (left == right && nums[left] >= s) {
+//                return 1;
+//            }
+//            sum = nums[left];
+//            right = left;
+//            while (right < nums.length - 1) {
+//                right++;
+//                sum += nums[right];
+//                if (sum >= s) {
+//                    res = Math.min(res, right - left + 1);
+//                    break;
+//                }
+//            }
+//        }
+//        if (res != Integer.MAX_VALUE) {
+//            return res;
+//        }
+//        return 0;
+//    }
+
+    //version 2.0:
+//    public int minSubArrayLen(int s, int[] nums) {
+//
+//    }
 
     public static void main(String[] args) {
         int[] nums = new int[]{2, 3, 1, 2, 4, 3};
