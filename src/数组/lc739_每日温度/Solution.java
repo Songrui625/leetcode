174f6af0bf3c017898b810145a108bd1e38b53f7
Songrui625 <1029229159@qq.com>
2020-09-12 21:58:54 +0800

    lc_739 每日温度

---
+++ src/数组/lc739_每日温度/Solution.java
@@ -0,0 +1,29 @@
+package 数组.lc739_每日温度;
+
+import java.util.Arrays;
+
+public class Solution {
+    //暴力求解：version:1.0
+//    public int[] dailyTemperatures(int[] T) {
+//        int n = T.length;
+//        int[] res = new int[n];
+//        for (int i = 0 ; i < n - 1; i++) {
+//            for (int j = i + 1; j < n; j++) {
+//                if (T[i] < T[j]) {
+//                    res[i] = j - i;
+//                    break;
+//                }
+//                if (j == n - 1) {
+//                    res[i] = 0;
+//                }
+//            }
+//        }
+//        return res;
+//    }
+    
+        public static void main(String[] args) {
+        int[] T = new int[]{73, 74, 75, 71, 69, 72, 76, 73};
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.dailyTemperatures(T)));
+    }
+}
---
+++ src/递归/lc110_平衡二叉树/Solution.java
@@ -0,0 +1,29 @@
+package 递归.lc110_平衡二叉树;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        } else {
+            return Math.abs(height(root.left) - height(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+        }
+    }
+
+    public int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            return Math.max(height(root.left), height(root.right)) + 1;
+        }
+    }
+}
